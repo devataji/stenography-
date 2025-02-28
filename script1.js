020b8cd393ac12f76ad366df913a390708a0f151
Devansh Gupta <work.devansh14@gmail.com>
2025-02-28 17:44:08 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,3 @@
-// Get elements
 const encodeButton = document.getElementById("encodeButton");
 const decodeButton = document.getElementById("decodeButton");
 const imageInput = document.getElementById("imageInput");
@@ -7,27 +6,26 @@ const canvas = document.getElementById("canvas");
 const decodedMessage = document.getElementById("decodedMessage");
 const ctx = canvas.getContext("2d");
 
-// Function to encode a message into an image
 function encodeMessage(imageData, message) {
     let binaryMessage = '';
     for (let i = 0; i < message.length; i++) {
         binaryMessage += message.charCodeAt(i).toString(2).padStart(8, '0');
     }
 
-    // Add a delimiter to signify the end of the message
-    binaryMessage += '1111111111111110'; // Custom delimiter for end of message
+    // Add a delimiter 
+    binaryMessage += '1111111111111110'; 
 
     let binaryIndex = 0;
 
-    // Encode message into the least significant bit of each pixel
+    // Encode message 
     for (let i = 0; i < imageData.data.length; i += 4) {
         if (binaryIndex < binaryMessage.length) {
-            // Set the least significant bit of each color channel
-            for (let j = 0; j < 3; j++) { // Loop through R, G, B channels
+            
+            for (let j = 0; j < 3; j++) { 
                 if (binaryMessage[binaryIndex] === '1') {
-                    imageData.data[i + j] |= 1; // Set LSB to 1
-                } else {
-                    imageData.data[i + j] &= ~1; // Set LSB to 0
+                    imageData.data[i + j] |= 1; 
+                } else {        
+                    imageData.data[i + j] &= ~1; 
                 }
                 binaryIndex++;
             }
@@ -37,17 +35,16 @@ function encodeMessage(imageData, message) {
     return imageData;
 }
 
-// Function to decode a hidden message from an image
 function decodeMessage(imageData) {
     let binaryMessage = '';
     for (let i = 0; i < imageData.data.length; i += 4) {
-        // Extract the LSB of each color channel
+    
         for (let j = 0; j < 3; j++) {
             binaryMessage += (imageData.data[i + j] & 1);
         }
     }
 
-    // Find the delimiter for end of the message
+ 
     const delimiter = '1111111111111110';
     const endOfMessageIndex = binaryMessage.indexOf(delimiter);
     if (endOfMessageIndex !== -1) {
@@ -63,7 +60,7 @@ function decodeMessage(imageData) {
     return 'No hidden message found.';
 }
 
-// Encode button click event
+
 encodeButton.addEventListener('click', () => {
     if (!imageInput.files[0] || !messageInput.value) {
         alert('Please select an image and enter a message.');
@@ -81,7 +78,7 @@ encodeButton.addEventListener('click', () => {
             const encodedData = encodeMessage(imageData, messageInput.value);
             ctx.putImageData(encodedData, 0, 0);
             alert('Message encoded in image. Now you can download the image.');
-            // Save the new image with the hidden message
+           
             const newImageUrl = canvas.toDataURL();
             const link = document.createElement('a');
             link.href = newImageUrl;
@@ -93,7 +90,6 @@ encodeButton.addEventListener('click', () => {
     fileReader.readAsDataURL(imageInput.files[0]);
 });
 
-// Decode button click event
 decodeButton.addEventListener('click', () => {
     if (!imageInput.files[0]) {
         alert('Please select an image to decode.');
